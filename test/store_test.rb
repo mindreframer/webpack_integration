dbd10301ed6436ba8158f86d723f20828c9e66f6
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-28 17:25:58 +0100

    asset keys

--- test/store_test.rb
+++ test/store_test.rb
@@ -13,4 +13,15 @@ describe 'WebpackIntegration::Store' do
       WebpackIntegration::Store.load
     end
   end
+
+  describe 'assets_keys' do
+    it "works" do
+      WebpackIntegration::Store.assets_keys
+    end
+
+    it "contains cleaned up file names, that you want to use in your application" do
+      expected = ["/brand_view-bundle.js", "/brand_view_styles-bundle.js", "/common.js", "/executive_view-bundle.js", "/executive_view_styles-bundle.js", "/img/logo.png", "/img/logo_for_runaways.png"]
+      WebpackIntegration::Store.assets_keys.must_equal expected
+    end
+  end
 end
