26910bd80c93da746aef05eb205eb07d10cfd49c
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-28 13:12:55 +0100

    main logic + some files

--- lib/webpack_integration.rb
+++ lib/webpack_integration.rb
@@ -1,5 +1,44 @@
 require "webpack_integration/version"
 
-module WebpackIntegration
-  # Your code goes here...
+module WebpackIntergration
+  # @example
+  #   WebpackIntergration.file_for('clients.css')
+  def self.file_for(filename)
+    assets_manifest.fetch(filename) # will raise on missing assets!
+  end
+
+  # @example
+  #   WebpackIntergration.fuzzy_file_for('client.png')
+  def self.fuzzy_file_for(file_pattern)
+    alternatives = assets_keys.grep(Regexp.new(file_pattern))
+    raise "TOO MANY MATCHES for #{file_pattern}" if alternatives.size > 1
+    file_for(alternatives.first)
+  end
+
+  def self.assets_keys
+    @assets_keys ||= assets_manifest.keys
+  end
+
+  # WebpackIntergration.assets_manifest
+  def self.assets_manifest
+    @assets_manifest ||= generate_assets_manifest
+  end
+
+  def self.generate_assets_manifest
+    res = {}
+    Dir['public/webpack/**/**/**'].each do |f|
+      next if File.directory?(f)
+      key      = f.gsub('public/webpack/', '')
+      key      = key.gsub(/-[a-f\d]*(\.)/, '.') # remove md5 hash
+      value    = f.sub('public', '')
+      res[key] = value
+    end
+    res
+  end
+
+  # should be called in development mode in after-request filter
+  def self.reset_assets_manifest!
+    @assets_manifest = nil
+    @assets_keys     = nil
+  end
 end
---
+++ lib/webpack_integration/view_helper.rb
@@ -0,0 +1,4 @@
+require 'webpack_integration'
+  def webpack_file(filename)
+    WebpackIntergration.fuzzy_file_for(filename)
+  end
