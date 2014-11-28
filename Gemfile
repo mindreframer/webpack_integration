34c0e5f796fc190ffbca42fb2563526c730af4d6
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-28 19:01:46 +0100

    code coverage

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in webpack_integration.gemspec
 gemspec
+
+gem 'codecov', :require => false, :group => :test
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,19 @@
 require 'bundler'
+
+require 'simplecov'
+SimpleCov.start do
+  add_filter "/test/"
+  add_filter "/.direnv/"
+end
+if ENV['CI']=='true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
+
 Bundler.setup
 
 require 'webpack_integration'
 require 'maxitest'
+require 'mocha/mini_test'
 require 'maxitest/autorun'
-
