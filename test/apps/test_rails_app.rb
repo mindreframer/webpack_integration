29736717bedf4b9144160c2b7a42a9aba73f55ce
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-29 16:12:43 +0100

    railtie test seems to work

--- test/apps/test_rails_app.rb
+++ test/apps/test_rails_app.rb
@@ -5,20 +5,19 @@ module TestRailsApp
   class Application < Rails::Application
     config.secret_token = '572c86f5ede338bd8aba8dae0fd3a326aabababc98d1e6ce34b9f5'
     config.eager_load   = false
+    routes.draw do
+      get  '/view_test' => 'test_rails_app/view_test#index'
+    end
   end
 
   class ViewTestController < ActionController::Base
     before_filter :set_view_path
     def index
-      # renders a partial with helpers
+      #render text: view_context.webpack_fuzzy_file('common.js')
     end
 
     def set_view_path
       self.view_paths.push(File.dirname(__FILE__))
     end
   end
-end
-TestRailsApp::Application.initialize!
-TestRailsApp::Application.routes.draw do
-  get  '/view_test' => 'test_rails_app/view_test#index'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,14 +10,21 @@ if ENV['CI']=='true'
   SimpleCov.formatter = SimpleCov::Formatter::Codecov
 end
 
-
+require 'pry'
 Bundler.setup
 
 require './test/apps/test_rails_app'
 require 'webpack_integration'
+
+# we need to initialize application after loading webpack_integration, so railtie hooks run!
+Rails.application.initialize!
+# somehow the routes are gone after init, but I dont care why, just re-init them
+TestRailsApp::Application.routes.draw do
+  get  '/view_test' => 'test_rails_app/view_test#index'
+end
+
+
 require 'rack/test'
 require 'maxitest'
 require 'mocha/mini_test'
 require 'maxitest/autorun'
-
-require 'pry'
