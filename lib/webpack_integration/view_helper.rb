1db5a68e201bc5a28b3c047dc886656b9e12c9f2
Roman Heinrich <roman.heinrich@gmail.com>
2014-11-29 10:59:45 +0100

    keep view helper method names consistent, webpack_fuzzy_file (fuzzy match)  + webpack_file (exact match)

--- lib/webpack_integration/view_helper.rb
+++ lib/webpack_integration/view_helper.rb
@@ -1,7 +1,11 @@
 module WebpackIntegration
   module ViewHelper
-    def webpack_file(filename)
+    def webpack_fuzzy_file(filename)
       WebpackIntegration.fuzzy_file_for(filename)
+    end
+
+    def webpack_file(filename)
+      WebpackIntegration.file_for(filename)
     end
   end
 end
